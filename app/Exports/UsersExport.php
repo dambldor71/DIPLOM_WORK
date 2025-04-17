89ff6cefa26af469791821acd65ad4192b1f46ef
Dambldor71 <ya.mr-csega@yandex.ru>
2025-04-17 17:10:21 +0500

    Изменил Excel Export, привёл документу к некоторому шаблону.

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -3,26 +3,40 @@
 namespace App\Exports;
 
 use App\Models\Tender;
-use App\Models\WorkStageTendersModel;
 use Illuminate\Support\Facades\Auth;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithCustomStartCell;
+use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Border;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
-use Illuminate\Support\Collection;
+use PhpOffice\PhpSpreadsheet\Worksheet\PageSetup;
 
-class UsersExport implements FromCollection, WithHeadings, WithStyles, ShouldAutoSize
+class UsersExport implements FromCollection, WithCustomStartCell, WithHeadings, WithStyles, ShouldAutoSize, WithEvents
 {
     protected $userId;
     protected $uploadKind;
     protected  $needField;
+    protected $mainTitle;
 
-    public function __construct($userId, $uploadKind, $needField)
+    public function __construct($userId, $uploadKind, $needField, $mainTitle = null)
     {
         $this->userId = $userId;
         $this->uploadKind = $uploadKind;
         $this->needField = $needField;
+        $this->mainTitle = 'Отчёт по избранным тендерам';
+    }
+
+    /**
+     * Указываем, с какой ячейки начинать запись данных (после шапки)
+     */
+    public function startCell(): string
+    {
+        return 'A5';
     }
 
     /**
@@ -70,14 +84,74 @@ class UsersExport implements FromCollection, WithHeadings, WithStyles, ShouldAut
     public function styles(Worksheet $sheet): array
     {
         $highestRowAndColumn = $sheet->getHighestRowAndColumn();
-        $range = 'A1:' . $highestRowAndColumn['column'] . $highestRowAndColumn['row'];
+        $range = 'A5:' . $highestRowAndColumn['column'] . $highestRowAndColumn['row'];
 
         return [
-            1 => ['font' => ['bold' => true]], // Стилизация первой строки (заголовки)
+            1 => ['font' => ['name' => 'Times New Roman']],
+            'A3:D3' => ['font' => ['name' => 'Times New Roman']],
+            5 => ['font' => ['bold' => true]], // Стилизация первой строки (заголовки)
             $range => [
+                'borders' => [
+                    'allBorders' => [
+                        'borderStyle' => Border::BORDER_THIN,
+                        'color' => ['rgb' => '000000'], // Цвет границы (черный в данном случае)
+                    ],
+                ],
                 'font' => ['size' => 11, 'name' => 'Times New Roman'], // Пример стилизации
                 'alignment' => ['horizontal' => 'center', 'vertical' => 'center', 'wrapText' => true], // Пример стилизации
             ],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function writerProperties(): array
+    {
+        return [
+            'orientation' => PageSetup::ORIENTATION_LANDSCAPE, // Для альбомной ориентации
+            'paperSize'   => PageSetup::PAPERSIZE_A4,
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function(AfterSheet $event) {
+                $sheet = $event->sheet;
+
+                $highestRowAndColumn = $sheet->getHighestRowAndColumn();
+                $sheet->mergeCells('A1:' . $highestRowAndColumn['column'] . 2);
+                $sheet->mergeCells('A3:B3');
+                $sheet->mergeCells('C3:D3');
+                $sheet->mergeCells('A' . $highestRowAndColumn['row'] + 2 . ':' . $highestRowAndColumn['column'] . $highestRowAndColumn['row'] + 2);
+                // Записываем основной заголовок в объединенную ячейку
+                $sheet->setCellValue('A1', $this->mainTitle);
+                $sheet->setCellValue('A3', 'Дата: ' . date('d.m.Y'));
+                $sheet->setCellValue('C3', 'ООО "Инженерный центр Пумори"');
+                $sheet->setCellValue('A' . $highestRowAndColumn['row'] + 2, 'Подпись:__________');
+
+                // Стиль для основного заголовка
+                $sheet->getStyle('A1')->getFont()->setSize(18)->setBold(true);
+                $sheet->getStyle('A3')->getFont()->setSize(11)->setBold(true);
+                $sheet->getStyle('C3')->getFont()->setSize(11)->setBold(true);
+                $sheet->getStyle('A' . $highestRowAndColumn['row'] + 2)->getFont()->setName('Times New Roman')->setSize(11)->setBold(true);
+
+                $sheet->getStyle('A1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER)->setVertical(Alignment::VERTICAL_CENTER);
+                $sheet->getStyle('A3')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_LEFT)->setVertical(Alignment::VERTICAL_CENTER);
+                $sheet->getStyle('C3')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_LEFT)->setVertical(Alignment::VERTICAL_CENTER);
+                $sheet->getStyle('A' . $highestRowAndColumn['row'] + 2)->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT)->setVertical(Alignment::VERTICAL_CENTER);
+
+                $highestRow = $sheet->getHighestRow() - 2;
+
+                for ($header = 2; $header <= 5; $header++) {
+                    $sheet->getRowDimension($header)->setRowHeight(50);
+                }
+
+                for ($row = 6; $row <= $highestRow; $row++) {
+                    $sheet->getRowDimension($row)->setRowHeight(200);
+                }
+            },
         ];
     }
 }
